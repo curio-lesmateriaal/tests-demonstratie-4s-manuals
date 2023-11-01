1753cf5364054967bfe65109656650ed79f53d9e
luttje <2738114+luttje@users.noreply.github.com>
2023-11-01 11:49:00 +0100

    Erg simpel voorbeeld toegevoegd

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,13 +4,17 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+// Voorbeeld functie om te testen
+// Dit zou uiteraard niet de goeie plek zijn om deze functie te definiëren. Liever in een aparte file ergens in `app/
+function sum(int $a, int $b): int
+{
+    return $a + $b;
+}
+
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    public function test_that_the_sum_of_two_numbers_is_correct(): void
     {
-        $this->assertTrue(true);
+        $this->assertEquals(4, sum(2, 2));
     }
 }
